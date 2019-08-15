537344ffeeef81e4f5b1160646d4cee024e88a82
World Wide <agwalker249@gmail.com>
2019-08-15 17:00:41 -0500

    Worked on instagram component

--- client/src/components/Insta/Insta.js
+++ client/src/components/Insta/Insta.js
@@ -1,26 +1,34 @@
 import React from "react";
-import {Row} from 'reactstrap';
+import {Row, Col} from 'reactstrap';
+import { Link } from "react-router-dom";
 import "./Insta.css"
 
 const Insta = (props) => (
   <div className="instaContainer">
-    <Row id="btnRow">
-      <button>Follow Us on Instagram  </button> 
-      <i className="fab fa-instagram fa-3x"></i>
+    <Row >
+    <Col></Col>
+    <Col id="instaBtnCol">
+    <a href="https://www.instagram.com/l_e_dev/" target="_blank">
+    <div id="instaBtnBox">
+      <button id="instaBtn">Follow Us on Instagram </button> <i className="fab fa-instagram fa-2x icon"></i>
+    </div>
+    </a>
+    </Col>
+    <Col></Col>
     </Row>
     <Row id="instaRow1">
       {props.instaRow1.map(pic => (
         <div className="instaBox"key={pic.images.standard_resolution.url}>
-          <img className="imgBox"src={pic.images.standard_resolution.url} alt="instagram"/>
-          <h5>{pic.caption.text}</h5>
+          <a href={pic.link} target="_blank"><p className="caption">{pic.caption.text}</p></a>
+          <a className="imgBox" href={pic.link} target="_blank"> <img className="imgBox" src={pic.images.standard_resolution.url} alt="instagram"/></a>
         </div>
       ))}
     </Row>
     <Row id="instaRow2">
       {props.instaRow2.map(pic => (
         <div className="instaBox" key={pic.images.standard_resolution.url}>
-          <img className="imgBox"src={pic.images.standard_resolution.url} alt="instagram"/>
-          <h5>{pic.caption.text}</h5>
+         <a href={pic.link} target="_blank"><p className="caption">{pic.caption.text}</p></a>
+         <a className="imgBox" href={pic.link} target="_blank"> <img className="imgBox" src={pic.images.standard_resolution.url} alt="instagram"/></a>
         </div>
       ))}
     </Row>
